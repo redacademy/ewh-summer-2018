3c6533b3fbc2191753d65f52c9800687e2e61310
EmilioLombana <emiliolombana@gmail.com>
2018-09-06 15:34:38 -0700

    Add front-page.php

--- themes/ewh/js/hamburger.js
+++ themes/ewh/js/hamburger.js
@@ -3,7 +3,6 @@
 
   $('.hamburger').on('click', function() {
     $('.nav-screen').show();
-<<<<<<< HEAD
     $('.nav-screen').animate(
       { height: '100%', width: '100%' },
       500,
@@ -12,9 +11,6 @@
       }
     );
   }); 
-=======
-  });
->>>>>>> db66bcd8b4170971e2fff3cc9c6808bc65e40c19
 
   $('.exit-nav').on('click', function() {
     $('#page').show();
