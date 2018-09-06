4eba66ba9ae437777c31248ed8d28ff80210a9c1
Julia Browne <julia.browne@live.ca>
2018-09-06 16:52:56 -0700

    style abortion-methods page

--- themes/ewh/page-abortion-methods.php
+++ themes/ewh/page-abortion-methods.php
@@ -10,11 +10,15 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+			<div class="abortion-methods-page">
+			
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
 			<?php endwhile; // End of the loop. ?>
+
+			</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
