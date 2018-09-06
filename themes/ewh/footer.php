5792b095ebed09659a2715b7505ac0b8a4dd45be
Juett <juettdm@gmail.com>
2018-09-06 10:50:48 -0700

    corrected enqueue style for google font and phone button style

--- themes/ewh/footer.php
+++ themes/ewh/footer.php
@@ -9,24 +9,16 @@
 
 			</div><!-- #content -->
 
-			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Footer") ) : ?>
-<?php endif;?>
+	
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
-			<?php dynamic_sidebar('sidebar-1'); ?>
 
-				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
-				</div><!-- .site-info -->
-				<div class="phone-button container">
-				<a href="tel:6043226692" >
-				<img class="phone-icon" src="<?php echo get_template_directory_uri() . '/images/phone button.svg' ?>" class="logo" alt="phone-button" />
-				</a>
-				</div>
+			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("footer") ) : ?>
+			<?php dynamic_sidebar('footer'); ?>
+<?php endif;?>
+
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
-
-		<?php wp_footer(); ?>
 
 	</body>
 </html>
