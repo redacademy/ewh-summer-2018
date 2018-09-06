a67d7606e83188e7f821cfc8132b365acab13324
Safenorsound <ijroback@gmail.com>
2018-09-06 15:18:58 -0700

    update birth control styling

--- themes/ewh/page-birth-control.php
+++ themes/ewh/page-birth-control.php
@@ -8,7 +8,7 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="site-main-birth-control" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
@@ -16,78 +16,82 @@ get_header(); ?>
 
 			<?php endwhile; // End of the loop. ?>
 
+			<section class="birth-control container">
+
 			<div class="birth-control-methods-blue">
 		<h2> IUD </h2>
-		<p> 99.8% Effective
+		<p> 99.8% Effective <br>
 $300-400 </p>
 		</div>
 
 		<div class="birth-control-methods-blue">
 		<h2> Birth Control </h2>
-		<p> 99.8% Effective
+		<p> 99.8% Effective <br>
 $300-400 </p>
 		</div>
 
 		<div class="birth-control-methods-teal">
 		<h2> Birth Control Pills </h2>
-		<p> 99.8% Effective
+		<p> 99.8% Effective <br>
 $300-400 </p>
 		</div>
 
 		<div class="birth-control-methods-teal">
 		<h2> Birth Control Patch </h2>
-		<p> 99.8% Effective
+		<p> 99.8% Effective <br>
 $300-400 </p>
 		</div>
 
 		<div class="birth-control-methods-blue">
 		<h2> NuvaRing </h2>
-		<p> 99.8% Effective
+		<p> 99.8% Effective <br>
 $300-400 </p>
 		</div>
 
 		<div class="birth-control-methods-blue">
 		<h2> Condoms </h2>
-		<p> 99.8% Effective
+		<p> 99.8% Effective <br>
 $300-400 </p>
 		</div>
 
 
 		<div class="birth-control-methods-teal">
 		<h2> Spermicides </h2>
-		<p> 99.8% Effective
+		<p> 99.8% Effective <br>
 $300-400 </p>
 		</div>
 
 		<div class="birth-control-methods-teal">
 		<h2> Emergency Pills </h2>
-		<p> 99.8% Effective
+		<p> 99.8% Effective <br>
 $300-400 </p>
 		</div>
 
 		<div class="birth-control-methods-blue">
 		<h2> Vasectomy </h2>
-		<p> 99.8% Effective
+		<p> 99.8% Effective <br>
 $300-400 </p>
 		</div>
 
 		<div class="birth-control-methods-blue">
 		<h2> Tubal Ligation</h2>
-		<p> 99.8% Effective
+		<p> 99.8% Effective <br>
 $300-400 </p>
 		</div>
 
 		<div class="birth-control-methods-teal">
 		<h2> Withdrawal Method </h2>
-		<p> 99.8% Effective
+		<p> 99.8% Effective <br>
 $300-400 </p>
 		</div>
 
 		<div class="birth-control-methods-teal">
 		<h2> Calendar Method </h2>
-		<p> 99.8% Effective
+		<p> 99.8% Effective <br>
 $300-400 </p>
 		</div>
+
+		</section>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
