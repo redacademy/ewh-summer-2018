db66bcd8b4170971e2fff3cc9c6808bc65e40c19
Juett <juettdm@gmail.com>
2018-09-06 14:55:17 -0700

    styled nav-screen and phone-icon

--- themes/ewh/js/hamburger.js
+++ themes/ewh/js/hamburger.js
@@ -3,20 +3,12 @@
 
   $('.hamburger').on('click', function() {
     $('.nav-screen').show();
-    $('.nav-screen').animate(
-      { height: '100%', width: '100%' },
-      500,
-      function() {
-        $('#page').hide();
-      }
-    );
   });
 
   $('.exit-nav').on('click', function() {
     $('#page').show();
-    $('.nav-screen').animate({ height: '0', width: '0' }, 500, function() {
       $('.nav-screen').hide();
-      $('.home').animate({ height: '100%', width: '100%' }, 500);
-    });
   });
+
+
 })(jQuery);
