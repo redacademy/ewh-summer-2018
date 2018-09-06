234c35d3c66974aa60954047d2134c5db7f1ad60
Juett <juettdm@gmail.com>
2018-09-05 21:20:21 -0700

    enqueue hamburger.js

--- themes/ewh/js/hamburger.js
+++ themes/ewh/js/hamburger.js
@@ -7,7 +7,6 @@
       { height: '100%', width: '100%' },
       500,
       function() {
-        // $('.menu-navigation-menu-container').css('display', 'block');
         $('#page').hide();
       }
     );
@@ -15,7 +14,6 @@
 
   $('.exit-nav').on('click', function() {
     $('#page').show();
-    // $('.menu-navigation-menu-container').css('display', 'none');
     $('.nav-screen').animate({ height: '0', width: '0' }, 500, function() {
       $('.nav-screen').hide();
       $('.home').animate({ height: '100%', width: '100%' }, 500);
