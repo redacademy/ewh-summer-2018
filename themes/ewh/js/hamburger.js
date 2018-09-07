c8df5b8caff3c0ae1f37ef7355b375fb6385a3ca
Juett <juettdm@gmail.com>
2018-09-07 01:13:32 -0700

    style nav, header, footer and added a couple variables

--- themes/ewh/js/hamburger.js
+++ themes/ewh/js/hamburger.js
@@ -1,19 +1,32 @@
 (function($) {
-  $('.nav-screen').hide();
   $('.exit-nav').hide();
-  $('menu-navigation-menu-container').hide();
 
   $('.hamburger').on('click', function() {
     $('.exit-nav').show();
-    $('menu-navigation-menu-container').show();
+    $('.hamburger').hide();
+    $('.site-header').toggleClass('nav-site-header');
+    $('.menu-navigation-menu-container').toggleClass('.menu-navigation-menu-container1');
+    $('.span-home-logo').toggleClass('nav-span-home-logo');
+    $('.span-chinese-nav').hide();
+    $('.menu-navigation-menu-container').show();
 
+    
   }); 
 
   $('.exit-nav').on('click', function() {
+    $('.site-header').toggleClass('nav-site-header');
+    $('.menu-navigation-menu-container').toggleClass('.menu-navigation-menu-container1');
+    $('.span-home-logo').toggleClass('nav-span-home-logo');
     $('.exit-nav').hide();
-    $('menu-navigation-menu-container').hide();
+    $('.hamburger').show();
+    $('.span-chinese-nav').show();
+    $('.menu-navigation-menu-container').hide();
 
 
+  });
+
+  $('.menu-item-61').on('click', function(){
+$('.menu-item-61').animate({height: '200px'});
   });
 
 
