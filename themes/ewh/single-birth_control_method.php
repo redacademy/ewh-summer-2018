24b236a4a4882110a22adfda34f3d627615488a2
Safenorsound <ijroback@gmail.com>
2018-09-10 16:03:01 -0700

    create fields for bcm title, img, and text in php

---
+++ themes/ewh/single-birth_control_method.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * The template for Birth Control Methods and Testimonials
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main-birth-control" role="main">
+
+			<section class="birth-control container">
+
+				<?php
+$loop = CFS()->get( 'products' );
+foreach ( $loop as $row ) {
+	$product_image = $row['product_image'];
+    echo "<h1 class='bcm-title'>" . $row['product_title'] . "</h1>";
+	echo "<img class = 'bcm-img' src='" . $product_image . "'/>";
+	echo "<p class ='bcm-text'>" .$row['product_text'] . "</p>";
+}?>
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+<?php get_template_part( 'template-parts/content', 'single' ); ?>
+
+<?php the_post_navigation(); ?>
+
+<?php
+	// If comments are open or we have at least one comment, load up the comment template.
+	if ( comments_open() || get_comments_number() ) :
+		comments_template();
+	endif;
+?>
+
+<?php endwhile; // End of the loop. ?>
+
+		</section>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
