e7da1bb12e13ab81a4b58b8d4bd4ecfd1747a067
Julia Browne <julia.browne@live.ca>
2018-09-11 16:50:20 -0700

    style your-decision page for tablet, style history page for mobile, tablet and desktop

--- themes/ewh/page-history.php
+++ themes/ewh/page-history.php
@@ -1,4 +1,3 @@
-
 <?php
 /**
  * The template for displaying all pages.
@@ -11,11 +10,21 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+
+			<?php if ( has_post_thumbnail() ) ?>
+    			<div class="your-decision-banner">
+				<?php the_post_thumbnail(); ?>
+				</div>
+
+			<div class="history-page container">
+				
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
 			<?php endwhile; // End of the loop. ?>
+
+			</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
