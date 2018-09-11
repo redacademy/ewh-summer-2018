90cff01278e2e95e80735b9f507019833c2ce6cb
Safenorsound <ijroback@gmail.com>
2018-09-10 17:00:05 -0700

    create feilds for bcm banner: title/img/text

--- themes/ewh/single-birth_control_method.php
+++ themes/ewh/single-birth_control_method.php
@@ -13,14 +13,22 @@ get_header(); ?>
 
 			<section class="birth-control container">
 
+				<div class="birth-control-header">
+
 				<?php
 $loop = CFS()->get( 'products' );
+
+if ($loop) {
+	
 foreach ( $loop as $row ) {
 	$product_image = $row['product_image'];
     echo "<h1 class='bcm-title'>" . $row['product_title'] . "</h1>";
 	echo "<img class = 'bcm-img' src='" . $product_image . "'/>";
 	echo "<p class ='bcm-text'>" .$row['product_text'] . "</p>";
+}
 }?>
+
+				</div>
 
 				<?php while ( have_posts() ) : the_post(); ?>
 
