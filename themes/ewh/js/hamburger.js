c5bf9194c0a7231c1dc82e92cedb5909bdef704c
Juett <juettdm@gmail.com>
2018-09-12 09:13:05 -0700

    correct side slide position and animation

--- themes/ewh/js/hamburger.js
+++ themes/ewh/js/hamburger.js
@@ -15,24 +15,34 @@
   }
   // Side Drop Down Reset
   function sideReset() {
-    $('.top-side-drop-down').animate({ width: '0px' }, 450, function() {
-      $('.menu-item').removeClass('blue');
-      $('.top-side-drop-down').slideUp(function() {
-        $('.sub-menu').removeClass('top-side-drop-down');
+      $('.top-side-drop-down').animate({ width: '0px' }, 350, function() {
+        $('.menu-item').removeClass('blue');
+        $('.top-side-drop-down').slideUp(function() {
+          $('.sub-menu').removeClass('top-side-drop-down');
+        });
       });
+      $('.side-drop-down').animate({ width: '0px' }, 350, function() {
+        $('.menu-item').removeClass('blue');
+        $('.side-drop-down').slideUp(function() {
+          $('.sub-menu').removeClass('side-drop-down');
+        });
+      });
+      setTimeout(function() {
+        $('.active').slideUp(250, function() {
+          $('.sub-menu').removeClass('active');
+        });
+      }, 200);
+      $('.menu-item-has-children').removeClass('angle-icon');
+  }
+  function activeReset() {
+    $('.active').slideUp(500, function() {
+      $('.sub-menu').removeClass('active');
     });
-    $('.side-drop-down').animate({ width: '0px' }, 450, function() {
-      $('.menu-item').removeClass('blue');
-      $('.side-drop-down').slideUp(function() {
-        $('.sub-menu').removeClass('side-drop-down');
-      });
+    $('.active2').slideUp(500, function() {
+      $('.sub-menu').removeClass('active2');
     });
-    setTimeout(function() {
-      $('.active').slideUp(400, function() {
-        $('.sub-menu').removeClass('active');
-        $('.menu-item-has-children').removeClass('angle-icon');
-      });
-    }, 50);
+    $('.menu-item-has-children').removeClass('angle-icon');
+    $('.menu-item').removeClass('blue');
   }
   //  Click on Hamburger Icon
   $('.hamburger').on('click', function() {
@@ -66,16 +76,16 @@
           });
         } else if ($('.menu > .menu-item')) {
           // Other Menus Reset
-          sideReset();
-          $('.active2').slideUp(500, function() {
-            $('.sub-menu').removeClass('active2');
-            $('.menu-item-has-children').removeClass('angle-icon');
-          });
+          activeReset()
+          setTimeout(function() {
+            clicked.parent().addClass('blue');
+            clicked.parent().addClass('angle-icon');
+            }, 150);
+          $('.menu-item').animate({ height: '25%' }, 0);
           // Outside Menus Slide Down
           clicked.parent().animate({ height: '100%' }, 0, function() {
             clicked.next('.sub-menu').slideDown(500, function() {
               clicked.next('.sub-menu').addClass('active');
-              clicked.parent().addClass('angle-icon');
               $('.sub-menu > .menu-item-has-children').addClass('side-slide');
             });
           });
@@ -83,30 +93,30 @@
         // Regular Header
         // Side Slide
       } else if (clicked.parent().hasClass('side-slide')) {
-        // if Top Menu Item
+        // if Top Menu Item Side Slide
         if (clicked.parent().index() === 0) {
           clicked.next('.sub-menu').addClass('top-side-drop-down');
           clicked.parent().addClass('blue');
           $('.top-side-drop-down').slideDown(0, function() {
             $('.top-side-drop-down').animate({ width: '200px' }, 300);
           });
-          // if Not Top Menu Item
+          // if Not Top Menu Item Side SLide
         } else {
           clicked.next('.sub-menu').addClass('side-drop-down');
           clicked.parent().addClass('blue');
           $('.side-drop-down').slideDown(0, function() {
-            $('.side-drop-down').animate({ width: '220px' }, 300);
+            $('.side-drop-down').animate({ width: '280px' }, 300);
           });
         }
       } else {
         //  Down Slide
         if ($('.menu > .menu-item')) {
           sideReset();
-          clicked.next('.sub-menu').animate({ width: '200px' }, 0, function() {
+          clicked.parent().addClass('angle-icon');
+            clicked.next('.sub-menu').animate({ width: '250px' }, 0, function() {
             clicked.next('.sub-menu').slideDown(500, function() {
-              clicked.next('.sub-menu').addClass('active');
-              clicked.parent().addClass('angle-icon');
               $('.sub-menu > .menu-item-has-children').addClass('side-slide');
+              clicked.next('.sub-menu').addClass('active');
             });
           });
         }
