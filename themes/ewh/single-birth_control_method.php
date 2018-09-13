870405e1fb5ceba16c0cfd44a3a1a3a55f6b7365
Safenorsound <ijroback@gmail.com>
2018-09-12 17:02:00 -0700

    update IUD header

--- themes/ewh/single-birth_control_method.php
+++ themes/ewh/single-birth_control_method.php
@@ -7,34 +7,21 @@
 
 get_header(); ?>
 
-
-	<div id="primary" class="content-area">
+	<div id="primary" class="single-bcm-content-area">
 		<main id="main" class="site-main-birth-control" role="main">
 
-			<section class="birth-control container">
-
-				<div class="birth-control-header">
-
-				<?php
-$loop = CFS()->get( 'products' );
-
-if ($loop) {
-	
-foreach ( $loop as $row ) {
-	$product_image = $row['product_image'];
-    echo "<h1 class='bcm-title'>" . $row['product_title'] . "</h1>";
-	echo "<img class = 'bcm-img' src='" . $product_image . "'/>";
-	echo "<p class ='bcm-text'>" .$row['product_text'] . "</p>";
-}
-}?>
-
-				</div>
+			<section class="birth-control-header">	
 
 				<?php while ( have_posts() ) : the_post(); ?>
 
 <?php get_template_part( 'template-parts/content', 'single' ); ?>
 
+<?php
+// echo CFS()->get "<p class ='bcm-text'>" .$row( 'head_line_text'). "</p>"; 
+?>
+
 <?php the_post_navigation(); ?>
+
 
 <?php
 	// If comments are open or we have at least one comment, load up the comment template.
@@ -46,6 +33,22 @@ foreach ( $loop as $row ) {
 <?php endwhile; // End of the loop. ?>
 
 		</section>
+
+		<div class="birth-control-container">
+
+	<?php
+	$loop = CFS()->get( 'products' );
+	if(!is_null($loop )){
+		foreach ( $loop as $row ) {
+			$product_image = $row['product_image'];
+			echo "<h1 class='bcm-title'>" . $row['product_title'] . "</h1>";
+			echo "<img class = 'bcm-img' src='" . $product_image . "'/>";
+			echo "<p class ='bcm-text'>" .$row['product_text'] . "</p>";
+		}
+	}
+	?>
+
+</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
