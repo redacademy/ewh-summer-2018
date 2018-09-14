8f045c9f3f252fae140e4d0771b0f69f4178bc48
EmilioLombana <emiliolombana@gmail.com>
2018-09-14 13:03:07 -0700

    Style testimonial page

--- themes/ewh/page-testimonial.php
+++ themes/ewh/page-testimonial.php
@@ -9,7 +9,11 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
+		<?php if ( has_post_thumbnail() ): ?>
+    			<div class="regular-banner" style="background-image: url('<?php the_post_thumbnail_url(); ?>');">
+				<h1 class="regular-title"> <?php the_title(); ?> </h1>
+				</div>
+			<?php endif; ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
